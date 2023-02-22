69081879f5a49b5b154764063e490405827c0bb9
Adriano Barbosa <barbosa.aob@gmail.com>
2023-02-22 09:09:02 -0400

    Add tests.

--- hello_world/tests.py
+++ hello_world/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import string
+import random
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class HelloWorldTest(TestCase):
+    def test_default_response(self):
+        c = Client()
+        response = c.get('/hello_world/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello world!<br>')
+
+    def test_msg_response(self):
+        c = Client()
+        response = c.get('/hello_world/?msg=abc123')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello world!<br>Your message was: abc123')
+
+        response = c.get('/hello_world/?msg=%21%40%23xyz')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello world!<br>Your message was: !@#xyz')
+
+    def test_404_response(self):
+        c = Client()
+        response = c.get('/hello_world/1')
+        self.assertEqual(response.status_code, 404)
+
+        response = c.get('/')
+        self.assertEqual(response.status_code, 404)
